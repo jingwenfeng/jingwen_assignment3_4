14e262db5ab461dfef3e109909cd7cf41fd4da9d
jingwenfeng <60762210+jingwenfeng@users.noreply.github.com>
2020-02-19 16:04:47 -0500

    Add files via upload

---
+++ 03_problem4.md
@@ -0,0 +1,7 @@
+| line | new-list                |
+|  1   | ('Jingwen', '', 'Feng') |
+|  2   |       Jingwen           |
+|  3   |       Feng              |     |
+|  4   |      ('', 'Feng')       |
+|  5   |     ('Jingwen', '')     |
+
---
+++ 03_problem4.py
@@ -0,0 +1,11 @@
+my_tuple = ('Jingwen', '', 'Feng')
+new_tuple1 = my_tuple
+new_tuple2 = my_tuple[0]
+new_tuple3 = my_tuple[-1]
+new_tuple4 = my_tuple[1:3]
+new_tuple5 = my_tuple[-3:-1]
+print(new_tuple1)
+print(new_tuple2)
+print(new_tuple3)
+print(new_tuple4)
+print(new_tuple5)
